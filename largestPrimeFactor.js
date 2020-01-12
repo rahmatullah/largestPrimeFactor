1c44dddeb3dc3f09427e0286789fa8507443055d
Rhmatullah Darwish <rahmat.darwish@gmail.com>
2020-01-12 15:23:30 +0430

    Create largestPrimeFactor.js

---
+++ largestPrimeFactor.js
@@ -0,0 +1,13 @@
+function largestPrimeFactor(n){
+var i=2;
+while (i<=n){
+    if (n%i == 0){
+        n/=i;    
+    }else{
+        i++;
+    }
+}
+return i;
+}
+
+console.log(largestPrimeFactor(9));
